c543ac3788fa29ee2c813f5ad12f13e65dc9fee4
Josiah Khoo <josiahkhooshaoqi@gmail.com>
2022-08-25 15:19:01 +0800

    SPA-12 Add operators

---
+++ Team03/Code03/src/spa/src/common/operators/equality_operator.h
@@ -0,0 +1,17 @@
+//
+// Created by Josiah Khoo on 25/8/22.
+//
+
+#ifndef SPA_EQUALITY_OPERATOR_H
+#define SPA_EQUALITY_OPERATOR_H
+
+enum EqualityOperator {
+  EQUALS,
+  NOT_EQUALS,
+  GREATER_THAN,
+  GREATER_THAN_OR_EQUALS,
+  LESSER_THAN,
+  LESSER_THAN_OR_EQUALS,
+};
+
+#endif // SPA_EQUALITY_OPERATOR_H
---
+++ Team03/Code03/src/spa/src/common/operators/logical_operator.h
@@ -0,0 +1,10 @@
+//
+// Created by Josiah Khoo on 25/8/22.
+//
+
+#ifndef SPA_MODIFIER_H
+#define SPA_MODIFIER_H
+
+enum LogicalOperator { AND, OR, INVERSE };
+
+#endif // SPA_MODIFIER_H
