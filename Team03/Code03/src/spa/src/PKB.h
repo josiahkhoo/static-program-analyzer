d221f0188b281f9bb4f8f2cb31c6b10dc618dd34
Josiah Khoo <josiahkhooshaoqi@gmail.com>
2022-08-25 15:20:40 +0800

    SPA-12 Make PKB implement QueryablePkb and StorablePkb

--- Team03/Code03/src/spa/src/PKB.h
+++ Team03/Code03/src/spa/src/PKB.h
@@ -1,21 +1,21 @@
 #pragma once
 
-#include<stdio.h>
+#include "queryable_pkb.h"
+#include "storable_pkb.h"
 #include <iostream>
+#include <stdio.h>
 #include <string>
 #include <vector>
 
-using namespace std;
 typedef short PROC;
 
 class TNode;
 
-class VarTable;  // no need to #include "VarTable.h" as all I need is pointer
+class VarTable; // no need to #include "VarTable.h" as all I need is pointer
 
-class PKB {
+class PKB : public QueryablePkb, public StorablePkb {
 public:
-	static VarTable* varTable; 
-	static int setProcToAST(PROC p, TNode* r);
-	static TNode* getRootAST (PROC p);
-
+  static VarTable *varTable;
+  static int setProcToAST(PROC p, TNode *r);
+  static TNode *getRootAST(PROC p);
 };
