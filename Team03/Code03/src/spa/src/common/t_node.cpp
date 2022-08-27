a434b19366fd1d817d299da74b108d626603566e
Josiah Khoo <josiahkhooshaoqi@gmail.com>
2022-08-27 11:04:14 +0800

    SPA-15 Add TNode implementation

--- Team03/Code03/src/spa/src/common/t_node.cpp
+++ Team03/Code03/src/spa/src/common/t_node.cpp
@@ -1,1 +1,60 @@
 #include "t_node.h"
+
+#include <utility>
+
+TNode::TNode(TNode::Type type, int line_number,
+             std::vector<std::shared_ptr<TNode>> children)
+    : type_(type), line_number_(line_number), children_(std::move(children)) {}
+
+TNode::TNode(TNode::Type type, int line_number, int int_val)
+    : type_(type), line_number_(line_number), maybe_int_(int_val) {}
+
+TNode::TNode(TNode::Type type, int line_number, std::string string_val)
+    : type_(type), line_number_(line_number), maybe_string_(string_val) {}
+
+int TNode::GetLine() const { return line_number_; }
+
+std::vector<std::shared_ptr<TNode>> TNode::GetChildren() const {
+  return children_;
+}
+
+TNode::Type TNode::GetType() const { return type_; }
+
+int TNode::GetIntValue() const {
+  assert(IsType(Constant) && maybe_int_.has_value());
+  return maybe_int_.value();
+}
+
+std::string TNode::GetStringValue() const {
+  assert(IsType(Program) || IsType(Procedure) || IsType(Variable));
+  assert(maybe_string_.has_value());
+  return maybe_string_.value();
+}
+
+bool TNode::IsStatement() const {
+  return IsType(Assign) || IsType(Call) || IsType(IfElseThen) ||
+         IsType(Print) || IsType(Read) || IsType(While);
+}
+
+bool TNode::IsEqualityOperator() const {
+  return IsType(Greater) || IsType(GreaterThanEqual) || IsType(Lesser) ||
+         IsType(LesserThanEqual) || IsType(Equal) || IsType(NotEqual);
+}
+
+bool TNode::IsLogicalOperator() const {
+  return IsType(And) || IsType(Or) || IsType(Inverse);
+}
+
+bool TNode::IsPlusMinusOperator() const {
+  return IsType(Plus) || IsType(Minus);
+}
+
+bool TNode::IsTimesDivideQuotientOperator() const {
+  return IsType(Multiply) || IsType(Divide) || IsType(Modulo);
+}
+
+bool TNode::IsLeaf() const { return IsType(Variable) || IsType(Constant); }
+
+bool TNode::IsType(TNode::Type type) const { return type_ == type; }
+
+bool TNode::IsNotType(TNode::Type type) const { return !IsType(type); }
--- Team03/Code03/src/spa/src/common/t_node.h
+++ Team03/Code03/src/spa/src/common/t_node.h
@@ -2,6 +2,7 @@
 #define SPA_T_NODE_H
 
 #include <optional>
+#include <string>
 #include <vector>
 
 class TNode {
@@ -19,6 +20,7 @@ class TNode {
     Not,
     And,
     Or,
+    Inverse,
     Greater,
     GreaterThanEqual,
     Lesser,
@@ -34,17 +36,31 @@ class TNode {
     Constant,
   };
 
-  int GetLine() const;
-  std::vector<TNode> GetChildren() const;
-  Type GetType() const;
-  std::optional<int> GetIntValue() const;
-  std::optional<std::string> GetStringValue() const;
-  bool IsStatement() const;
-  bool IsExpression() const;
-  bool IsOperator() const;
-  bool IsLeaf() const;
-  bool IsType(Type type) const;
-  bool IsNotType(Type type) const;
+  TNode(Type type, int line_number,
+        std::vector<std::shared_ptr<TNode>> children);
+  TNode(Type type, int line_number, int int_val);
+  TNode(Type type, int line_number, std::string string_val);
+
+  [[nodiscard]] int GetLine() const;
+  [[nodiscard]] std::vector<std::shared_ptr<TNode>> GetChildren() const;
+  [[nodiscard]] Type GetType() const;
+  [[nodiscard]] int GetIntValue() const;
+  [[nodiscard]] std::string GetStringValue() const;
+  [[nodiscard]] bool IsStatement() const;
+  [[nodiscard]] bool IsEqualityOperator() const;
+  [[nodiscard]] bool IsLogicalOperator() const;
+  [[nodiscard]] bool IsPlusMinusOperator() const;
+  [[nodiscard]] bool IsTimesDivideQuotientOperator() const;
+  [[nodiscard]] bool IsLeaf() const;
+  [[nodiscard]] bool IsType(Type type) const;
+  [[nodiscard]] bool IsNotType(Type type) const;
+
+ private:
+  Type type_;
+  int line_number_;
+  std::vector<std::shared_ptr<TNode>> children_;
+  std::optional<int> maybe_int_;
+  std::optional<std::string> maybe_string_;
 };
 
 #endif  // SPA_T_NODE_H
