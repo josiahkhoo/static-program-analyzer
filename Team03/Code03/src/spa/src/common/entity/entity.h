71300e707913c526f26cc2cff51b109170b57107
Josiah Khoo <josiahkhooshaoqi@gmail.com>
2022-08-28 10:30:54 +0800

    SPA-37 Update stringable import on entity to statement_entity

--- Team03/Code03/src/spa/src/common/entity/entity.h
+++ Team03/Code03/src/spa/src/common/entity/entity.h
@@ -1,7 +1,6 @@
 #ifndef SPA_ENTITY_H
 #define SPA_ENTITY_H
 
-#include "common/stringable.h"
 #include "has_node_pointer.h"
 
 class Entity : public HasNodePointer {
--- Team03/Code03/src/spa/src/common/entity/statement_entity.h
+++ Team03/Code03/src/spa/src/common/entity/statement_entity.h
@@ -1,6 +1,7 @@
 #ifndef SPA_STATEMENT_ENTITY_H
 #define SPA_STATEMENT_ENTITY_H
 
+#include "common/stringable.h"
 #include "entity.h"
 
 class StatementEntity : public Entity, public Stringable {
