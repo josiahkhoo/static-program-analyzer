8d1d8c724f2ecd8b75cad35bc0d46c6f078bc8b4
Josiah Khoo <josiahkhooshaoqi@gmail.com>
2022-08-29 15:38:58 +0800

    SPA-24 Add automated autotester

--- Team03/Code03/src/spa/src/sp/extractor/entity_extractor.cpp
+++ Team03/Code03/src/spa/src/sp/extractor/entity_extractor.cpp
@@ -123,7 +123,7 @@ void EntityExtractor::RecursivelyExtractNode(
               if_entities, print_entities, procedure_entities, read_entities,
               statement_entities, variable_entities, while_entities);
   if (!node.IsLeaf()) {
-    for (std::shared_ptr<TNode> child : node.GetChildren()) {
+    for (const std::shared_ptr<TNode>& child : node.GetChildren()) {
       RecursivelyExtractNode(
           *child, assign_entities, call_entities, constant_entities,
           if_entities, print_entities, procedure_entities, read_entities,
---
+++ autoautotester.py
@@ -0,0 +1,59 @@
+import argparse
+import os
+
+# Parser
+from collections import Counter
+
+parser = argparse.ArgumentParser(description="Runs autotester")
+parser.add_argument("--autotester_path", help="Autotester path", required=True)
+parser.add_argument("--test_dir", help="Tests folder", required=True)
+parser.add_argument("--os", help="Tests folder", choices=["mac", "windows"], required=True)
+args = parser.parse_args()
+
+# Test arguments
+autotester_path: str = args.autotester_path
+test_dir_arg: str = args.test_dir
+os_type: str = args.os
+
+# Scripts
+test_array = (
+    ("Entities Only", "entities_only"),
+    ("Semantic Errors", "semantic_errors"),
+    ("Syntax Errors", "syntax_errors"),
+)
+
+overall_passed_test_cases = 0
+overall_failed_test_cases = 0
+
+for test in test_array:
+    test_name = test[0]
+    test_dir = test[1]
+    source_path = f"{test_dir_arg}/{test_dir}/test_{test_dir}_source.txt"
+    queries_path = f"{test_dir_arg}/{test_dir}/test_{test_dir}_queries.txt"
+    output_path = f"{test_dir_arg}/out.xml"
+    print(f"--- Running {test_name} Test ---")
+    command = f"{autotester_path} {source_path} {queries_path} {output_path}"
+    if os_type == 'windows':
+        command = command.replace("/", "\\")
+        command += "> nul"
+        output_path = output_path.replace("/", "\\")
+    else:
+        command += "> /dev/null"
+    os.system(command)
+    with open(output_path) as f:
+        output = f.read()
+        text_chunks = Counter(output.split())
+        passed_test_cases = text_chunks['<passed>']
+        failed_test_cases = text_chunks['<failed>']
+        overall_passed_test_cases += overall_passed_test_cases
+        overall_failed_test_cases += failed_test_cases
+        print(f"Passed: {passed_test_cases}, Failed: {failed_test_cases}")
+
+print("=== Test Summary ===")
+print(f"Total Passed: {overall_passed_test_cases}")
+print(f"Total Failed: {overall_failed_test_cases}")
+
+if overall_failed_test_cases > 0:
+    exit(1)
+
+exit(0)
