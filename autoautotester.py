3f2b1878d60fe05c43e0b4b179a8815ab6646c69
Josiah Khoo <josiahkhooshaoqi@gmail.com>
2022-09-05 19:20:13 +0800

    SPA-49 Add sprint 1 autotest cases and add debug flag to autoautotester.py

--- autoautotester.py
+++ autoautotester.py
@@ -8,12 +8,14 @@ parser = argparse.ArgumentParser(description="Runs autotester")
 parser.add_argument("--autotester_path", help="Autotester path", required=True)
 parser.add_argument("--test_dir", help="Tests folder", required=True)
 parser.add_argument("--os", help="Tests folder", choices=["mac", "windows"], required=True)
+parser.add_argument("--debug", help="Debug flag", choices=["true", "false"], default=False)
 args = parser.parse_args()
 
 # Test arguments
 autotester_path: str = args.autotester_path
 test_dir_arg: str = args.test_dir
 os_type: str = args.os
+debug: str = args.debug
 
 # Scripts
 # Comment out tests that you don't want to test
@@ -21,6 +23,7 @@ test_array = (
     # ("Entities Only", "entities_only"),
     # ("Semantic Errors", "semantic_errors"),
     # ("Syntax Errors", "syntax_errors"),
+    # ("Sprint 1", "sprint_1"),
 )
 
 overall_passed_test_cases = 0
@@ -31,15 +34,18 @@ for test in test_array:
     test_dir = test[1]
     source_path = f"{test_dir_arg}/{test_dir}/test_{test_dir}_source.txt"
     queries_path = f"{test_dir_arg}/{test_dir}/test_{test_dir}_queries.txt"
+    print(source_path, queries_path)
     output_path = f"{test_dir_arg}/out.xml"
     print(f"--- Running {test_name} Test ---")
     command = f"{autotester_path} {source_path} {queries_path} {output_path}"
     if os_type == 'windows':
         command = command.replace("/", "\\")
-        command += "> nul"
+        if not debug:
+            command += "> nul"
         output_path = output_path.replace("/", "\\")
-    # else:
-    #     command += "> /dev/null"
+    else:
+        if not debug:
+            command += "> /dev/null"
     os.system(command)
     with open(output_path) as f:
         output = f.read()
