83b7adbaa5d777cb9c74f98af588cbf1970f7692
LAPTOP-VPVQTRMT\jerar <jerard.soh@gmail.com>
2022-09-06 17:19:50 +0800

    SPA-50 Implement parent abstraction extractor

---
+++ Team03/Code03/src/spa/src/sp/extractor/abstraction/parent_abstraction_extractor.cpp
@@ -0,0 +1,47 @@
+#include "parent_abstraction_extractor.h"
+
+std::vector<ParentAbstraction> ParentAbstractionExtractor::Extract(
+    const std::vector<AssignEntity> &assign_entities,
+    const std::vector<CallEntity> &call_entities,
+    const std::vector<ConstantEntity> &constant_entities,
+    const std::vector<IfEntity> &if_entities,
+    const std::vector<PrintEntity> &print_entities,
+    const std::vector<ProcedureEntity> &procedure_entities,
+    const std::vector<ReadEntity> &read_entities,
+    const std::vector<StatementEntity> &statement_entities,
+    const std::vector<VariableEntity> &variable_entities,
+    const std::vector<WhileEntity> &while_entities,
+    std::unordered_map<TNode, StatementEntity *> &t_node_stmt_ent_umap,
+    std::unordered_map<TNode, VariableEntity *> &t_node_var_ent_umap,
+    std::unordered_map<TNode, ConstantEntity *> &t_node_const_ent_umap) const {
+  std::vector<ParentAbstraction> parent_abstractions = {};
+
+  for (const auto &while_entity : while_entities) {
+    // Hacky way of retrieving while statements children, by assuming that it
+    // has 2 children: {while_cond, stmt_list}
+    auto lhs = t_node_stmt_ent_umap[*while_entity.GetNodePointer()];
+    auto children = while_entity.GetNodePointer()->GetChildren()[1]->GetChildren();
+    for (int i = 0; i < (int)children.size() - 1; i++) {
+      auto rhs = t_node_stmt_ent_umap[*children[i]];
+      parent_abstractions.emplace_back(lhs, rhs);
+    }
+  }
+  for (const auto &if_entity : if_entities) {
+    // Hacky way of retrieving the then and else statements,
+    // Assumes that If Entity has 3 children, {if, then, else}
+    auto lhs = t_node_stmt_ent_umap[*if_entity.GetNodePointer()];
+    std::vector<std::shared_ptr<TNode>> then_statements =
+        if_entity.GetNodePointer()->GetChildren()[1]->GetChildren();
+    for (int i = 0; i < (int)then_statements.size() - 1; i++) {
+      auto rhs = t_node_stmt_ent_umap[*then_statements[i]];
+      parent_abstractions.emplace_back(lhs, rhs);
+    }
+    std::vector<std::shared_ptr<TNode>> else_statements =
+        if_entity.GetNodePointer()->GetChildren()[2]->GetChildren();
+    for (int i = 0; i < (int)else_statements.size() - 1; i++) {
+      auto rhs = t_node_stmt_ent_umap[*else_statements[i]];
+      parent_abstractions.emplace_back(lhs, rhs);
+    }
+  }
+  return parent_abstractions;
+}
