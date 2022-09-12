859c2f26008d07c97fce7163c34fa7f96de2d6be
Xingjian <xingjian.chen@yahoo.com.sg>
2022-09-12 10:45:04 +0800

    SPA-53 Add unit testing for query pattern bracketization

---
+++ Team03/Code03/src/unit_testing/src/spa/pkb/pattern_store/test_query_pattern_processor.cpp
@@ -0,0 +1,22 @@
+#include "catch.hpp"
+#include "pkb/pattern_store/query_pattern_processor.h"
+
+TEST_CASE("Assign Pattern Bracketization", "[QueryPatternProcessor]") {
+  QueryPatternProcessor processor;
+
+  std::string test_pattern_1 = "x+y+z";
+  std::string processed_pattern_1 = "(((x)+(y))+(z))";
+  REQUIRE(processed_pattern_1.compare(processor.ProcessAssignPattern(test_pattern_1)) == 0);
+
+  std::string test_pattern_2 = "x*y/z";
+  std::string processed_pattern_2 = "(((x)*(y))/(z))";
+  REQUIRE(processed_pattern_2.compare(processor.ProcessAssignPattern(test_pattern_2)) == 0);
+
+  std::string test_pattern_3 = "x+y*z";
+  std::string processed_pattern_3 = "((x)+((y)*(z)))";
+  REQUIRE(processed_pattern_3.compare(processor.ProcessAssignPattern(test_pattern_3)) == 0);
+
+  std::string test_pattern_4 = "v-x*y+z*t";
+  std::string processed_pattern_4 = "(((v)-((x)*(y)))+((z)*(t)))";
+  REQUIRE(processed_pattern_4.compare(processor.ProcessAssignPattern(test_pattern_4)) == 0);
+}
