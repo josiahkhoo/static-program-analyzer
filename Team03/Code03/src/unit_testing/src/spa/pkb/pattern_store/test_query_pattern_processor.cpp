12e08fa7ea6ce90b0df7ac4d92b95737d9321d25
Xingjian <xingjian.chen@yahoo.com.sg>
2022-09-12 10:47:22 +0800

    SPA-53 Reformat for clang

--- Team03/Code03/src/unit_testing/src/spa/pkb/pattern_store/test_query_pattern_processor.cpp
+++ Team03/Code03/src/unit_testing/src/spa/pkb/pattern_store/test_query_pattern_processor.cpp
@@ -6,17 +6,21 @@ TEST_CASE("Assign Pattern Bracketization", "[QueryPatternProcessor]") {
 
   std::string test_pattern_1 = "x+y+z";
   std::string processed_pattern_1 = "(((x)+(y))+(z))";
-  REQUIRE(processed_pattern_1.compare(processor.ProcessAssignPattern(test_pattern_1)) == 0);
+  REQUIRE(processed_pattern_1.compare(
+              processor.ProcessAssignPattern(test_pattern_1)) == 0);
 
   std::string test_pattern_2 = "x*y/z";
   std::string processed_pattern_2 = "(((x)*(y))/(z))";
-  REQUIRE(processed_pattern_2.compare(processor.ProcessAssignPattern(test_pattern_2)) == 0);
+  REQUIRE(processed_pattern_2.compare(
+              processor.ProcessAssignPattern(test_pattern_2)) == 0);
 
   std::string test_pattern_3 = "x+y*z";
   std::string processed_pattern_3 = "((x)+((y)*(z)))";
-  REQUIRE(processed_pattern_3.compare(processor.ProcessAssignPattern(test_pattern_3)) == 0);
+  REQUIRE(processed_pattern_3.compare(
+              processor.ProcessAssignPattern(test_pattern_3)) == 0);
 
   std::string test_pattern_4 = "v-x*y+z*t";
   std::string processed_pattern_4 = "(((v)-((x)*(y)))+((z)*(t)))";
-  REQUIRE(processed_pattern_4.compare(processor.ProcessAssignPattern(test_pattern_4)) == 0);
+  REQUIRE(processed_pattern_4.compare(
+              processor.ProcessAssignPattern(test_pattern_4)) == 0);
 }
