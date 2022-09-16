c0057b9c150d4507d925d8eb00047be2c54c7f15
KohHanMing <kohhanming1997@gmail.com>
2022-09-16 19:12:58 +0800

    SPA-59 Updated PKB for uses store and query

--- Team03/Code03/src/spa/src/PKB.h
+++ Team03/Code03/src/spa/src/PKB.h
@@ -124,6 +124,18 @@ class PKB : public QueryablePkb, public StorablePkb {
                                                Expression exp) const override;
   std::unordered_set<std::string> QueryPattern(std::string lhs,
                                                Expression exp) const override;
+  std::unordered_set<std::string> GetUsesP() const override;
+  std::unordered_set<std::string> GetUsesP(
+      std::string uses_name) const override;
+  std::unordered_set<std::string> GetUsesPBy() const override;
+  std::unordered_set<std::string> GetUsesPBy(
+      std::string variable_name) const override;
+  std::unordered_set<std::string> GetUsesS() const override;
+  std::unordered_set<std::string> GetUsesS(
+      std::string uses_name) const override;
+  std::unordered_set<std::string> GetUsesSBy() const override;
+  std::unordered_set<std::string> GetUsesSBy(
+      std::string variable_name) const override;
 
  private:
   EntityManager entity_manager_;
