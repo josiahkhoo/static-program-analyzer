b822c64c526ec61bf9ec70133dad45fc6f07a714
Xingjian <xingjian.chen@yahoo.com.sg>
2022-09-19 04:14:37 +0800

    SPA-90 Implement support for pattern expressions containing brackets (Needs more unit testing on method to make sure it works)

--- Team03/Code03/src/spa/src/pkb/pattern_store/query_pattern_processor.h
+++ Team03/Code03/src/spa/src/pkb/pattern_store/query_pattern_processor.h
@@ -1,6 +1,11 @@
 #ifndef SPA_QUERY_PATTERN_PROCESSOR_H
 #define SPA_QUERY_PATTERN_PROCESSOR_H
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+
 #include "string"
 
 class QueryPatternProcessor {
@@ -8,7 +13,11 @@ class QueryPatternProcessor {
   std::string ProcessAssignPattern(std::string exp) const;
 
  private:
+  std::string ProcessAssignPatternHelper(std::string exp) const;
+  std::set<int> GetOuterBracketIndexes(int exp_length, std::vector<std::pair<int, int>> pairs) const;
   bool CheckContains(std::string exp, char *arr, unsigned int n) const;
+  bool CheckContains(std::string exp, std::set<int> outer_bracket_indexes, char *arr, unsigned int n) const;
+  bool IsOperator(char c) const;
 };
 
 #endif  // SPA_QUERY_PATTERN_PROCESSOR_H
