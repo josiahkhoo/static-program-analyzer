2012f557c55c8537fd6d8ad294f5759146361a35
LAPTOP-VPVQTRMT\jerar <jerard.soh@gmail.com>
2022-09-21 16:12:52 +0800

    Add calls and callsT abstraction

---
+++ Team03/Code03/src/spa/src/common/abstraction/calls_abstraction.h
@@ -0,0 +1,25 @@
+#ifndef SPA_CALLS_ABSTRACTION_H
+#define SPA_CALLS_ABSTRACTION_H
+
+#include "abstraction.h"
+#include "common/entity/procedure_entity.h"
+
+class CallsAbstraction
+    : public Abstraction<ProcedureEntity, ProcedureEntity> {
+ public:
+  CallsAbstraction(ProcedureEntity lhs, ProcedureEntity rhs);
+
+  const ProcedureEntity &GetLeftHandSide() const override;
+
+  const ProcedureEntity &GetRightHandSide() const override;
+
+  bool operator==(const CallsAbstraction &rhs) const;
+
+  bool operator!=(const CallsAbstraction &rhs) const;
+
+ private:
+  ProcedureEntity lhs_;
+  ProcedureEntity rhs_;
+};
+
+#endif  // SPA_CALLS_ABSTRACTION_H
---
+++ Team03/Code03/src/spa/src/common/abstraction/calls_t_abstraction.h
@@ -0,0 +1,25 @@
+#ifndef SPA_CALLS_T_ABSTRACTION_H
+#define SPA_CALLS_T_ABSTRACTION_H
+
+#include "abstraction.h"
+#include "common/entity/procedure_entity.h"
+
+class CallsTAbstraction
+    : public Abstraction<ProcedureEntity, ProcedureEntity> {
+ public:
+  CallsTAbstraction(ProcedureEntity lhs, ProcedureEntity rhs);
+
+  const ProcedureEntity &GetLeftHandSide() const override;
+
+  const ProcedureEntity &GetRightHandSide() const override;
+
+  bool operator==(const CallsTAbstraction &rhs) const;
+
+  bool operator!=(const CallsTAbstraction &rhs) const;
+
+ private:
+  ProcedureEntity lhs_;
+  ProcedureEntity rhs_;
+};
+
+#endif  // SPA_CALLS_T_ABSTRACTION_H
